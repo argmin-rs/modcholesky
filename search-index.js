1c753c8e55ef1a25eedbad72c04b2772f4b6e475
Stefan Kroboth <stefan.kroboth@gmail.com>
2018-12-01 07:54:30 +0100

    Semiautomatic update of documentation

---
+++ search-index.js
@@ -0,0 +1,3 @@
+var N = null;var searchIndex = {};
+searchIndex["modcholesky"]={"doc":"Modified Cholesky decompositions","items":[[8,"ModCholeskySchnabel1","modcholesky","",N,N],[16,"L","","",0,N],[10,"mod_cholesky_schnabel1","","",0,[[["self"]],["result",["error"]]]]],"paths":[[8,"ModCholeskySchnabel1"]]};
+initSearch(searchIndex);
