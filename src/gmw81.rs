6b8a468594967f8bdafcbdfa6a63585ad941113f
Stefan Kroboth <stefan.kroboth@gmail.com>
2018-12-05 16:41:26 +0100

    gmw81 is now performed inplace

--- src/gmw81.rs
+++ src/gmw81.rs
@@ -25,23 +25,12 @@ pub trait ModCholeskyGMW81
 where
     Self: Sized,
 {
-    fn mod_cholesky_gmw81(&self) -> Result<(Self, Self, Self), Error>;
+    fn mod_cholesky_gmw81(&mut self) -> Result<(), Error>;
 }
 
 impl ModCholeskyGMW81 for ndarray::Array2<f64> {
     /// Algorithm 6.5 in "Numerical Optimization" by Nocedal and Wright
-    ///
-    /// This can certainly be implemented more memory efficiently
-    fn mod_cholesky_gmw81(
-        &self,
-    ) -> Result<
-        (
-            ndarray::Array2<f64>,
-            ndarray::Array2<f64>,
-            ndarray::Array2<f64>,
-        ),
-        Error,
-    > {
+    fn mod_cholesky_gmw81(&mut self) -> Result<(), Error> {
         use ndarray::s;
         debug_assert!(self.is_square());
         let n = self.raw_dim()[0];
@@ -64,7 +53,6 @@ impl ModCholeskyGMW81 for ndarray::Array2<f64> {
 
         let mut c: ndarray::Array2<f64> = ndarray::Array2::zeros(self.raw_dim());
         c.diag_mut().assign(&a_diag);
-        let mut l: ndarray::Array2<f64> = ndarray::Array::eye(n);
         let mut d: ndarray::Array1<f64> = ndarray::Array::zeros(n);
 
         for j in 0..n {
@@ -72,26 +60,28 @@ impl ModCholeskyGMW81 for ndarray::Array2<f64> {
             swap_rows(&mut c, j, j + max_idx);
             swap_columns(&mut c, j, j + max_idx);
             for s in 0..j {
-                l[(j, s)] = c[(j, s)] / d[s];
+                self[(j, s)] = c[(j, s)] / d[s];
             }
 
             for i in j..n {
                 c[(i, j)] =
-                    self[(i, j)] - (&l.slice(s![j, 0..j]) * &c.slice(s![i, 0..j])).scalar_sum();
+                    self[(i, j)] - (&self.slice(s![j, 0..j]) * &c.slice(s![i, 0..j])).scalar_sum();
             }
 
-            let theta =
-                if j < (n - 1) {
-                    c.slice(s![(j + 1).., j]).fold(0.0, |acc, x| {
-                        if (*x).abs() > acc {
-                            (*x).abs()
+            let theta = if j < (n - 1) {
+                c.slice(s![(j + 1).., j]).fold(
+                    0.0,
+                    |acc, &x| {
+                        if x.abs() > acc {
+                            x.abs()
                         } else {
                             acc
                         }
-                    })
-                } else {
-                    0.0
-                };
+                    },
+                )
+            } else {
+                0.0
+            };
 
             d[j] = c[(j, j)].abs().max((theta / beta).powi(2)).max(delta);
 
@@ -103,8 +93,15 @@ impl ModCholeskyGMW81 for ndarray::Array2<f64> {
             }
         }
         let mut dout = ndarray::Array2::eye(n);
-        dout.diag_mut().assign(&d);
-        Ok((l, dout, c))
+        dout.diag_mut()
+            .assign(&(d.iter().map(|x| x.sqrt()).collect::<ndarray::Array1<f64>>()));
+
+        // Set diagonal to ones
+        self.diag_mut().assign(&ndarray::Array1::ones(n));
+
+        // multiply with dout and return
+        *self = self.dot(&dout);
+        Ok(())
     }
 }
 
@@ -113,13 +110,17 @@ mod tests {
     use super::*;
     #[test]
     fn test_modified_cholesky() {
-        let a: ndarray::Array2<f64> =
+        let mut a: ndarray::Array2<f64> =
             ndarray::arr2(&[[4.0, 2.0, 1.0], [2.0, 6.0, 3.0], [1.0, 3.0, -0.004]]);
-        let (l, d, _) = a.mod_cholesky_gmw81().unwrap();
-        let f = l.dot(&d).dot(&(l.t()));
+        a.mod_cholesky_gmw81().unwrap();
+        // set upper triangle off diagonals to zero because its just garbage there
+        a[(0, 1)] = 0.0;
+        a[(0, 2)] = 0.0;
+        a[(1, 2)] = 0.0;
+        let f = a.dot(&(a.t()));
         let res: ndarray::Array2<f64> =
             ndarray::arr2(&[[4.0, 2.0, 1.0], [2.0, 6.0, 3.0], [1.0, 3.0, 3.004]]);
-        assert!(f.all_close(&res, 2.0 * std::f64::EPSILON));
+        assert!(f.all_close(&res, 1e-9));
         // let dsqrt = d.map(|x| x.sqrt());
         // let m = l.dot(&dsqrt);
         // println!("l: {:?}", l);
