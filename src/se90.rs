6b6252464404fa8e2df18c3cd36f59a947ce416c
Stefan Kroboth <stefan.kroboth@gmail.com>
2018-12-16 10:34:55 +0100

    more tests, not all working yet...

--- src/se90.rs
+++ src/se90.rs
@@ -62,7 +62,8 @@ impl ModCholeskySE90 for ndarray::Array2<f64> {
                 swap_columns(&mut l, j, j + max_idx);
                 swap_rows(&mut p, j, j + max_idx);
             }
-            let tmp = (j + 1..n).fold(std::f64::INFINITY, |acc, i| {
+            // println!("111: j: {}; oth: {}", j, j + max_idx);
+            let tmp = ((j + 1)..n).fold(std::f64::INFINITY, |acc, i| {
                 let nv = l[(i, i)] - l[(i, j)].powi(2) / l[(j, j)];
                 if nv < acc {
                     nv
@@ -79,7 +80,7 @@ impl ModCholeskySE90 for ndarray::Array2<f64> {
                 l[(j, j)] = l[(j, j)].sqrt();
                 for i in (j + 1)..n {
                     l[(i, j)] /= l[(j, j)];
-                    for k in (j + 1)..(i + 1) {
+                    for k in (j + 1)..=i {
                         l[(i, k)] -= l[(i, j)] * l[(k, j)];
                         // TEST
                         l[(k, i)] = l[(i, k)];
@@ -89,7 +90,6 @@ impl ModCholeskySE90 for ndarray::Array2<f64> {
             }
         }
 
-        // let mut delta;
         let mut delta_prev = 0.0;
 
         // Phase two, `self` not positive-definite
@@ -113,6 +113,7 @@ impl ModCholeskySE90 for ndarray::Array2<f64> {
                     swap_columns(&mut l, j, j + max_idx);
                     swap_rows(&mut p, j, j + max_idx);
                 }
+                // println!("222: j: {}; oth: {}", j, j + max_idx);
 
                 // Calculate E_jj and add to diagonal
                 let normj = l.slice(s![(j + 1).., j]).map(|x| x.abs()).scalar_sum();
@@ -125,18 +126,18 @@ impl ModCholeskySE90 for ndarray::Array2<f64> {
                 }
 
                 // Update Gershgorin bound estimates
-                if (l[(j, j)] - normj).abs() > 100.0 * std::f64::EPSILON {
+                if (l[(j, j)] - normj).abs() > 1.0 * std::f64::EPSILON {
                     let tmp = 1.0 - normj / l[(j, j)];
                     for i in (j + 1)..n {
                         g[i] += l[(i, j)].abs() * tmp;
                     }
                 }
 
-                // perform jth iteration of factorization
+                // Perform jth iteration of factorization
                 l[(j, j)] = l[(j, j)].sqrt();
                 for i in (j + 1)..n {
                     l[(i, j)] /= l[(j, j)];
-                    for k in (j + 1)..(i + 1) {
+                    for k in (j + 1)..=i {
                         l[(i, k)] -= l[(i, j)] * l[(k, j)];
                         // TEST
                         l[(k, i)] = l[(i, k)];
@@ -161,7 +162,7 @@ impl ModCholeskySE90 for ndarray::Array2<f64> {
         }
 
         // Make lower triangular
-        for i in 0..n {
+        for i in 0..(n - 1) {
             l.slice_mut(s![i, (i + 1)..]).fill(0.0);
         }
 
@@ -177,7 +178,12 @@ mod tests {
     fn test_modified_cholesky_se90() {
         let a: ndarray::Array2<f64> =
             ndarray::arr2(&[[1.0, 1.0, 2.0], [1.0, 1.0, 3.0], [2.0, 3.0, 1.0]]);
-        let res = ndarray::arr2(&[[3.0, 1.0, 2.0], [1.0, 3.2197, 3.0], [2.0, 3.0, 3.2197]]);
+        let res_l_up: ndarray::Array2<f64> = ndarray::arr2(&[
+            [1.732050807568877, 0.5773502691896257, 1.154700538379251],
+            [0.0, 1.698920954907997, 1.37342077428181],
+            [0.0, 0.0, 0.006912871809428971],
+        ]);
+        let res = res_l_up.t().dot(&res_l_up);
         let decomp = a.mod_cholesky_se90().unwrap();
         let l = decomp.l;
         let e = decomp.e;
@@ -190,7 +196,108 @@ mod tests {
         // println!("LLT:\n{:?}", l.dot(&l.t()));
         // println!("P*A*P^T + P*E*P^T:\n{:?}", paptpept);
         // println!("RES:\n{:?}", res);
-        assert!(paptpept.all_close(&l.dot(&l.t()), 1e-4));
-        assert!(l.dot(&(l.t())).all_close(&res, 1e-4));
+        assert!(paptpept.all_close(&l.dot(&l.t()), 1e-12));
+        assert!(l.dot(&(l.t())).all_close(&res, 1e-12));
     }
+
+    #[test]
+    fn test_modified_cholesky_se90_difficult() {
+        let a: ndarray::Array2<f64> = ndarray::arr2(&[
+            [1890.3, -1705.6, -315.8, 3000.3],
+            [-1705.6, 1538.3, 284.9, -2706.6],
+            [-315.8, 284.9, 52.5, -501.2],
+            [3000.3, -2706.6, -501.2, 4760.8],
+        ]);
+        let res_l_up: ndarray::Array2<f64> = ndarray::arr2(&[
+            [
+                33.19487912314187,
+                -15.09871441738697,
+                8.582649117145946,
+                -9.513515588608952,
+            ],
+            [0.0, 74.71431471239089, -34.4915568315879, 38.23441539976376],
+            [0.0, 0.0, 36.39190351527789, -8.386049991060743],
+            [0.0, 0.0, 0.0, 36.29044868461993],
+        ]);
+        let res = res_l_up.t().dot(&res_l_up);
+
+        let decomp = a.mod_cholesky_se90().unwrap();
+        let l = decomp.l;
+        let e = decomp.e;
+        let p = decomp.p;
+        let paptpept = p.dot(&a.dot(&p.t())) + p.dot(&e.dot(&p.t()));
+        // println!("A:\n{:?}", a);
+        // println!("L:\n{:?}", l);
+        // println!("E:\n{:?}", e);
+        // println!("P:\n{:?}", p);
+        // println!("LLT:\n{:?}", l.dot(&l.t()));
+        // println!("P*A*P^T + P*E*P^T:\n{:?}", paptpept);
+        // println!("RES:\n{:?}", res);
+        assert!(paptpept.all_close(&l.dot(&l.t()), 1e-12));
+        assert!(l.dot(&(l.t())).all_close(&res, 1e-12));
+    }
+
+    // #[test]
+    // fn test_modified_cholesky_se90_6x6() {
+    //     let a: ndarray::Array2<f64> = ndarray::arr2(&[
+    //         [14.8253, -6.4243, 7.8746, -1.2498, 10.2733, 10.2733],
+    //         [-6.4243, 15.1024, -1.1155, -0.2761, -8.2117, -8.2117],
+    //         [7.8746, -1.1155, 51.8519, -23.3482, 12.5902, 12.5902],
+    //         [-1.2498, -0.2761, -23.3482, 22.7967, -9.8958, -9.8958],
+    //         [10.2733, -8.2117, 12.5902, -9.8958, 21.0656, 21.0656],
+    //         [10.2733, -8.2117, 12.5902, -9.8958, 21.0656, 21.0656],
+    //     ]);
+    //     let res_l_up: ndarray::Array2<f64> = ndarray::arr2(&[
+    //         [
+    //             7.200826341469429,
+    //             -3.242433422611255,
+    //             -0.1549127762706699,
+    //             1.093568935922023,
+    //             1.748438221248757,
+    //             1.748438221248757,
+    //         ],
+    //         [
+    //             0.0,
+    //             3.504757552232888,
+    //             -0.2220964936286686,
+    //             0.6551164905259115,
+    //             -1.205962298692896,
+    //             -1.205962298692896,
+    //         ],
+    //         [
+    //             0.0,
+    //             0.0,
+    //             4.746236644186862,
+    //             -1.287207862277906,
+    //             -1.729514390954478,
+    //             -1.729514390954478,
+    //         ],
+    //         [
+    //             0.0,
+    //             0.0,
+    //             0.0,
+    //             4.363600851232103,
+    //             1.587006643784825,
+    //             1.587006643784825,
+    //         ],
+    //         [0.0, 0.0, 0.0, 0.0, 4.306053379607389, 2.564856408184844],
+    //         [0.0, 0.0, 0.0, 0.0, 0.0, 3.458844794641899],
+    //     ]);
+    //     let res = res_l_up.t().dot(&res_l_up);
+    //
+    //     let decomp = a.mod_cholesky_se90().unwrap();
+    //     let l = decomp.l;
+    //     let e = decomp.e;
+    //     let p = decomp.p;
+    //     let paptpept = p.dot(&a.dot(&p.t())) + p.dot(&e.dot(&p.t()));
+    //     println!("A:\n{:?}", a);
+    //     println!("L:\n{:?}", l);
+    //     println!("E:\n{:?}", e);
+    //     println!("P:\n{:?}", p);
+    //     println!("LLT:\n{:?}", l.dot(&l.t()));
+    //     println!("P*A*P^T + P*E*P^T:\n{:?}", paptpept);
+    //     println!("RES:\n{:?}", res);
+    //     // assert!(paptpept.all_close(&l.dot(&l.t()), 1e-3));
+    //     assert!(l.dot(&(l.t())).all_close(&res, 1e-3));
+    // }
 }
