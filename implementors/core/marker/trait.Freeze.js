7dd5acaffd0b8cd81e482c1499463351ddfaa933
Stefan Kroboth <stefan.kroboth@gmail.com>
2021-07-20 07:25:31 +0200

    Semiautomatic update of documentation

--- implementors/core/marker/trait.Freeze.js
+++ implementors/core/marker/trait.Freeze.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["modcholesky"] = [{"text":"impl&lt;L, E, P&gt; Freeze for Decomposition&lt;L, E, P&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;E: Freeze,<br>&nbsp;&nbsp;&nbsp;&nbsp;L: Freeze,<br>&nbsp;&nbsp;&nbsp;&nbsp;P: Freeze,&nbsp;</span>","synthetic":true,"types":[]}];
+implementors["modcholesky"] = [{"text":"impl&lt;L, E, P&gt; Freeze for <a class=\"struct\" href=\"modcholesky/struct.Decomposition.html\" title=\"struct modcholesky::Decomposition\">Decomposition</a>&lt;L, E, P&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;E: Freeze,<br>&nbsp;&nbsp;&nbsp;&nbsp;L: Freeze,<br>&nbsp;&nbsp;&nbsp;&nbsp;P: Freeze,&nbsp;</span>","synthetic":true,"types":["modcholesky::Decomposition"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
